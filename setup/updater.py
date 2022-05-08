e8e62140e47aacc79495fbb7bd46a0dc227ba12d
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-05-08 02:23:18 -0700

    Update updater.py

--- setup/updater.py
+++ setup/updater.py
@@ -42,7 +42,7 @@ async def update_requirements(main , test):
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f" 💥©༒ʟɛɢɛռɖ✘-թπο💥 {i}")
+            print(f" 💥©༒SaTya✘-թπο💥 {i}")
     except Exception as e:
         print(f"Error while installing requirments {str(e)}")
 
