b0f07e866727c7a33f74a1521b15e0ea81f1f6f9
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-05-08 02:30:24 -0700

    Create userbot.js

---
+++ userbot.js
@@ -0,0 +1,8 @@
+from userbot input java scripts 
+run :
+ await 
+  retune : 
+               legendbot run gitclone==int(legendbot("op install userbot ")))
+               
+               legendsatya=+int=op*)-int-str-mir-mod-RUNPY(LEGEND-SATYABOY)
+pass:
